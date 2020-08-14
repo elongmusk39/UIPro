e68a46c77c02d3d8082bf039eacb2857875792f1
Long Nguyen <elongmusk39@gmail.com>
2020-08-14 11:05:21 -0700

    Initial Commit

---
+++ UIPro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPro
+//
+//  Created by Long Nguyen on 8/14/20.
+//  Copyright © 2020 Long Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
